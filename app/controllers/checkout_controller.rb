5335f49b88577e6b73dde86d04517391a8b99110
leandroKachu <lesilvajunior@hotmail.com>
2023-02-07 22:55:16 -0300

    return secret key lol

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,7 +3,7 @@ class CheckoutController < ApplicationController
 
   def create
     require 'stripe'
-    Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+    Stripe.api_key = 'sk_test_51MWmjkHFC97yK2BpCHN44H3B5OarVE1biaZjaGbOz0MOH8aN8kw0283EjVIucUqJ52QvUZYtxzkNe7zLOGx2DJ5a00plhTltaZ'
     if params[:id].to_i.positive?
       product = Product.find_by(id:params[:id].to_i)
       @session=Stripe::Checkout::Session.create({
