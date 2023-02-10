e84dde756daf65dd7f20ba2e7115c77b4e754336
leandroKachu <lesilvajunior@hotmail.com>
2023-02-10 15:46:56 -0300

    adjust model user, front-html and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  def to_s
+    email
+  end
+
+  after_create do
+    binding.pry
+    customer = Stripe::Customer.create(email: email)
+    update(stripe_customer_id: customer.id)
+  end
 end
